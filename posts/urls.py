cf136569976536dcb9f69fbb2978781b91887229
EdlanPolicarpio <edlanpolicarpioschool@gmail.com>
2017-08-07 15:36:04 +1000

    Added "/posts"

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,9 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
+
     #Home page
-    url(r'^$',views.index,name='index')
+    url(r'^$',views.index,name='index'),
+    url(r'^posts/$', views.BlogListView.as_view(), name='blog-list'),
+    url(r'^post/(?P<slug>[-\w\d]+)', views.post, name='post')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from . import models
 
 # Create your views here.
+class BlogListView(ListView):
+    queryset = models.Entry.objects.published()
+    paginate_by = 20
+
 def index(request):
-	return render(request, 'posts/index.html')
+    return render(request, 'posts/index.html')
+
+def post(request,slug):
+    return render(request, 'posts/index.html')
