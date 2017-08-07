106211a2e2f65d287484c1e4c32277d4f75b257d
EdlanPolicarpio <edlanpolicarpioschool@gmail.com>
2017-08-07 18:38:49 +1000

    Can now create and edit entries.

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
 
     #Home page
     url(r'^$',views.index,name='index'),
-    url(r'^posts/$', views.BlogListView.as_view(), name='blog-list'),
-    url(r'^post/(?P<slug>[-\w\d]+)', views.post, name='post')
+    url(r'^entries/$', views.EntryListView.as_view(), name='entry-list'),
+    url(r'^entry/(?P<slug>[-\w\d]+)', views.entry, name='entry'),
+    url(r'^create/',views.create, name='create'),
+    url(r'^edit/(?P<slug>[-\w\d]+)', views.edit, name='edit')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.views.generic.list import ListView
 from . import models
+from . import forms
 
 # Create your views here.
-class BlogListView(ListView):
+class EntryListView(ListView):
     queryset = models.Entry.objects.published()
     paginate_by = 20
 
 def index(request):
     return render(request, 'posts/index.html')
 
-def post(request,slug):
-    return render(request, 'posts/index.html')
+def entry(request,slug):
+    entry = get_object_or_404(models.Entry, slug=slug)
+    context = {"entry":entry}
+    return render(request, 'posts/entry.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        #create form
+        form = forms.EntryForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return HttpResponseRedirect('/entries')
+    else:
+        form = forms.EntryForm() 
+    context = {'form':form}
+    return render(request, 'posts/create.html',context)
+
+def edit(request,slug):
+    entry = get_object_or_404(models.Entry, slug=slug)
+    if request.method == 'POST':
+        form = forms.EntryForm(instance = entry, data = request.POST)
+        if form.is_valid:
+            form.save()
+            return HttpResponseRedirect('/entries')
+    else:
+        init = {"title":entry.title, "body":entry.body, 
+	"published":entry.published, "slug":entry.slug}
+        form = forms.EntryForm(instance = entry, initial = init)
+
+    context = {"form":form, "entry":entry}
+    return render(request, 'posts/edit.html', context)
